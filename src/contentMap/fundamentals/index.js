8902fc381c56a57fc736585a5995bf5025725b9a
carles sanz mateu <carlessanzmateu@MacBook-Pro-de-carles.local>
2020-01-29 18:52:05 +0100

    feat(contentMap): folder and structure reorganization

---
+++ src/contentMap/fundamentals/index.js
@@ -0,0 +1,15 @@
+import basicNotation from "./fundamentals/basicNotation";
+
+export default {
+  basicNotation: basicNotation,
+  meter: {},
+  protonotation: {},
+  rhythmicValues: {},
+  beamsAndBorrowedDivisions: {},
+  pitches: {},
+  scalesAndScaleDegrees: {},
+  keySignatures: {},
+  intervals: {},
+  triadsAndSeventhChords: {},
+  typesOfMotion: {}
+};
