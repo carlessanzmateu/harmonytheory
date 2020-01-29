4595b041845a5170b9454a80a51452e1d08471c9
Pablo <psirera4@gmail.com>
2020-01-29 19:28:00 +0100

    create table of content component

---
+++ src/views/home/components/TableOfContent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Table Of Content</h1>
+    <section class="items-content">
+      <h2>Fundamentals</h2>
+      <ul>
+        <li>
+          <a class="link" @click="goToContent()">Basic notation</a>
+        </li>
+        <li>
+          <a class="link" @click="goToContent()">Meter</a>
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableOfContent',
+  props: {
+    data: {
+      type: Object
+    }
+  },
+  methods: {
+    goToContent() {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.items-content {
+  width: 40%;
+  margin: 0 auto;
+  text-align: left;
+
+  .link {
+    cursor: pointer;
+    color: #42b983;
+
+    &:hover {
+      font-weight: bold;
+    }
+  }
+}
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/logo.png" />
+    <TableOfContent :data="data" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+    TableOfContent: () => import('@/views/home/components/TableOfContent')
+  },
+  data: () => ({
+    data: {
+      fundamentals: {
+        basicNotation: {
+          title: {
+            componentType: 'topTitle',
+            translationKey: 'FOO.BAR.QUZ'
+          }
+        }
+      }
+    }
+  })
+}
+</script>
