13eeaac93894a8b028798b48242fe7ebb7d0cdd4
Carles Sanz Mateu <carlessanzmateu@gmail.com>
2020-01-29 19:09:57 +0100

    feat(content): create map (#3)
    * feat(content): create map
    
    * feat(contentMap): refactor recursive structure
    
    * feat(contentMap): improve recursivity for child content
    
    * feat(contentMap): folder and structure reorganization
    
    * style(contentMap): remove double quote
    
    * style(contentMap): lint fix
    
    * styles(contentMap): add ES6 style on object asignation

---
+++ src/contentMap/fundamentals/basicNotation.js
@@ -0,0 +1,113 @@
+export default {
+  title: {
+    componentType: 'topTitle',
+    translationKey: 'FOO.BAR.QUZ'
+  },
+  content: [
+    [
+      {
+        componentType: 'contentTitle',
+        translationKey: 'FOO.BAR.QUZ'
+      },
+      {
+        componentType: 'contentText',
+        translationKey: 'FOO.BAR.QUZ'
+      },
+      {
+        componentType: 'contentImage',
+        translationKey: 'FOO.BAR.QUZ'
+      }
+    ],
+    [
+      {
+        componentType: 'contentTitle',
+        translationKey: 'FOO.BAR.QUZ'
+      },
+      {
+        componentType: 'contentText',
+        translationKey: 'FOO.BAR.QUZ'
+      }
+    ],
+    [
+      {
+        componentType: 'contentTitle',
+        translationKey: 'FOO.BAR.QUZ'
+      },
+      {
+        componentType: 'contentText',
+        translationKey: 'FOO.BAR.QUZ'
+      },
+      {
+        componentType: 'contentImage',
+        translationKey: 'FOO.BAR.QUZ'
+      }
+    ],
+    [
+      {
+        componentType: 'contentTitle',
+        translationKey: 'FOO.BAR.QUZ'
+      },
+      {
+        componentType: 'contentText',
+        translationKey: 'FOO.BAR.QUZ'
+      }
+    ],
+    [
+      {
+        componentType: 'contentTitle',
+        translationKey: 'FOO.BAR.QUZ'
+      },
+      {
+        componentType: 'contentText',
+        translationKey: 'FOO.BAR.QUZ'
+      },
+      {
+        componentType: 'contentImage',
+        translationKey: 'FOO.BAR.QUZ'
+      }
+    ],
+    [
+      {
+        componentType: 'contentTitle',
+        translationKey: 'FOO.BAR.QUZ'
+      },
+      {
+        componentType: 'contentText',
+        translationKey: 'FOO.BAR.QUZ'
+      },
+      {
+        componentType: 'contentList',
+        elements: [
+          {
+            componentType: 'contentListElement',
+            translationKey: 'FOO.BAR.QUZ'
+          },
+          {
+            componentType: 'contentListElement',
+            translationKey: 'FOO.BAR.QUZ'
+          },
+          {
+            componentType: 'contentListElement',
+            translationKey: 'FOO.BAR.QUZ'
+          },
+          {
+            componentType: 'contentListElement',
+            translationKey: 'FOO.BAR.QUZ'
+          },
+          {
+            componentType: 'contentListElement',
+            translationKey: 'FOO.BAR.QUZ'
+          }
+        ]
+      },
+      {
+        componentType: 'contentText',
+        translationKey: 'FOO.BAR.QUZ'
+      },
+      {
+        componentType: 'contentImage',
+        translationKey: 'FOO.BAR.QUZ'
+      }
+    ]
+  ]
+}
---
+++ src/contentMap/index.js
@@ -0,0 +1,156 @@
+import fundamentals from './fundamentals'
+
+const contentMap = {
+  introduction: {},
+  fundamentals,
+  voiceLeadingAndModelComposition: {
+    introductionToStricVoiceLeading: {},
+    strictTwoVoiceComposition: {
+      composingACantusFirmus: {},
+      composingAFirstSpeciesCounterpoint: {},
+      composingASecondSpeciesCounterpoint: {},
+      composingAThirdSpeciesCounterpoint: {},
+      composingAFourthSpeciesCounterpoint: {}
+    },
+    strictFourVoiceComposition: {
+      introductionToThoroughbass: {},
+      aBriefHistoryOfBassoContinuo: {},
+      generatingRomanNumeralsFromAFiguredBassLine: {},
+      composingInBassoContinuoStyle: {},
+      styleAndTendency: {},
+      tendencyTonesAndFunctionalHarmonicDissonances: {},
+      realizingAFiguredBassInStrictBassoContinuoStyleVideo: {},
+      melodicKeyboardStyleVoiceLeading: {},
+      melodicKeyboardStyleVoiceLeadingSchemata: {},
+      realizingAFiguredBassLineInMelodicKeyboardStyleVideo: {},
+      realizingAnUnfiguredBassLineVideo: {},
+      embellishingTones: {}
+    },
+    harmony: {
+      introductionToMusicalFunctions: {},
+      harmonicFunctions: {},
+      harmonicSyntaxTheIdealizedPhrase: {},
+      harmonicSyntaxProlongation: {},
+      performingAHarmonicAnalysis: {},
+      classicalCadenceTypes: {},
+      chromaticallyAlteredSubdominantChords: {},
+      appliedChords: {},
+      modalMixture: {},
+      modulation: {},
+      handoutsAndCharts: {
+        leadSheetAndFiguredBassSymbols: {},
+        harmoniesByBassScaleDegree: {}
+      }
+    },
+    form: {
+      thematicStructureInTheClassicalStyle: {
+        classicalThemeTypes: {
+          theSentence: {},
+          thePeriod: {},
+          hybridThemes: {},
+          compoundPeriods: {},
+          compoundSentences: {},
+          theSmallTernary: {},
+          theSmallBinary: {},
+          themeTypeReference: {},
+          thematicFunctionReference: {}
+        },
+        techniquesOfPhraseRhythm: {
+          externalExpansions: {},
+          internalExpansions: {}
+        }
+      },
+      sonataForm: {
+        introductionToSonataTheory: {},
+        sonataFormEexpositionTypes: {},
+        structuralPointsOfArrival: {},
+        thematicModules: {},
+        sonataFormTheRecapitulation: {},
+        sonataFormTheDevelopment: {},
+        sonataFormFramingModulesIintroAndCoda: {}
+      },
+      rondoForm: {
+        introductionToRondoForm: {},
+        thematicFunctionInRondoForm: {},
+        fivePartRondo: {},
+        sonataRondo: {}
+      },
+      otherFormalStructuresInTheClassicalStyle: {
+        minuetForm: {}
+      },
+      galantSchemata: {
+        galantSchemataOpensAndCloses: {},
+        galantSchemataContinuationPatterns: {},
+        galantSchemataSummary: {},
+        improvisingASentenceWithGalantSchemata: {}
+      }
+    },
+    postTonalMusic: {
+      basics: {
+        pitchClass: {},
+        intervalClass: {},
+        modularTwelveArithmetic: {}
+      },
+      organizingForces: {
+        collectionsAndScales: {},
+        symmetryAndCentricity: {}
+      },
+      setTheory: {
+        pitchClassSets: {},
+        normalOrder: {},
+        tranposition: {},
+        inversion: {},
+        setClassAndPrimeFormOne: {},
+        setClassAndPrimeFormTwo: {},
+        complements: {},
+        commonTonesUnderTransposition: {},
+        commonTonesUnderInversion: {},
+        tranpositionalSymmetry: {}
+      },
+      twelveToneTheory: {
+        basics: {},
+        operations: {},
+        intervallicStructure: {},
+        derivation: {},
+        invariance: {}
+      },
+      generalPostTonalResources: {
+        analyzingAPostTonalPieceFromScratch: {},
+        analyzingAtonalMusic: {},
+        analyzingTwelveToneMusic: {},
+        glossaryOfAtonalMusicalTerms: {},
+        sheetOfBlankChromaticScaleClockFaces: {}
+      }
+    },
+    popRockMusic: {
+      rhythm: {
+        syncopationInPopRockMusic: {}
+      },
+      harmony: {
+        harmonyInPopRockMusic: {},
+        theFiftysDooWopProgression: {},
+        theSingerSongwriterProgression: {},
+        thePuffProgression: {},
+        theBluesProgression: {},
+        theLamentProgression: {},
+        theCircleOfFifthsProgressionInMinor: {},
+        plagalProgressions: {}
+      },
+      form: {
+        formInPopRockMusicOverview: {},
+        terminologyAndBasicConcepts: {},
+        formalContainersAndModuleStructures: {},
+        formalFunctions: {},
+        analyticalNotation: {}
+      }
+    },
+    textAndMusic: {
+      analyzingPoetry: {}
+    },
+    technology: {
+      usingTrinketInteractiveMusicNotation: {}
+    }
+  }
+}
+
+export default contentMap
